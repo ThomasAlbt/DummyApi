eaa8e16113ae25ad55ab0d8db3735300796ae37f
ThomasAlbt <thomas.albert.jv@gmail.com>
2025-08-15 15:48:21 +0100

    still big wip

--- routes/Views.js
+++ routes/Views.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Container = require('../models/Container');
+const Container = require("../models/Container");
 
 // Vercel needs to do it himself apparently
 // router.get('/', (req, res) => {
@@ -9,37 +9,57 @@ const Container = require('../models/Container');
 
 //Adding an item or incrementing the click
 
-router.post('/:id', async (req, res) => {
-    try {
-        const id = Number(req.params.id);
-        let container = await Container.findOne();
+router.post("/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
 
-        if (!container) {
-            return res.status(404).json({ error: 'No container found' });
-        }
+    let container = await Container.findOne();
 
-        let item = container.items.find((it) => it._id === id);
-        if (!item) {
-            return res.status(404).json({ error: 'Item not found' });
-        }
+    // If no container exists, create one
 
-        item.click = (item.click || 0) + 1;
-        await container.save();
-
-        res.json({ message: 'Click incremented', item });
-    } catch (error) {
-        console.error('Error incrementing click:', error);
-        res.status(500).json({ error: 'Internal server error' });
+    if (!container) {
+      container = new Container({ items: [] });
     }
+
+    // Try to find existing item
+
+    let item = container.items.find((it) => it._id === id);
+
+    if (!item) {
+      // Create new item if not found
+
+      item = {
+        _id: id,
+
+        click: 1, // start with 1 click
+
+        timeViewed: 0, // default time
+      };
+
+      container.items.push(item);
+    } else {
+      // Increment click if item exists
+
+      item.click = (item.click || 0) + 1;
+    }
+
+    await container.save();
+
+    res.json({ message: "Item updated or created", item });
+  } catch (error) {
+    console.error("Error processing item:", error);
+
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 //Getting the infos
 
-router.get('/:id', async (req, res) => {
-    let container = await Container.findOne();
-    const id = Number(req.params.id);
-    const result = container.items.find((items) => items._id === id);
-    res.send(result)
-})
+router.get("/:id", async (req, res) => {
+  let container = await Container.findOne();
+  const id = Number(req.params.id);
+  const result = container.items.find((items) => items._id === id);
+  res.send(result);
+});
 
 module.exports = router;
